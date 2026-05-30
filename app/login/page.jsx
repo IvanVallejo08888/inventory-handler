66ba15fc77fed0c789aadea7aa8758a519b9ec21
IvanVallejo08888 <vallejodavidi08@gmail.com>
2026-05-29 23:08:12 -0500

    Update page.jsx
    add space

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -5,6 +5,8 @@ import LoginForm from '@/components/LoginForm';
 
 export const metadata = { title: 'Iniciar Sesión — Área 17' };
 
+
+
 export default async function LoginPage() {
   const sesion = await obtenerSesion();
   if (sesion) redirect('/dashboard');
